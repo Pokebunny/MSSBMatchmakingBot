b2b12b173027aea5a7203cc1fffdad19b422363f
Pokebunny <pokebunnytaber@gmail.com>
2022-04-30 11:10:30 -0400

    Fix stars-on queue and message
    - Fix stars-on queue message posting
    - Fix stars-on rating boundaries to use the stars-on rating list
    - Add the matchmaking channel as a constant

--- MSSBMatchmakingBot.py
+++ MSSBMatchmakingBot.py
@@ -24,9 +24,11 @@ stars_off_sheet = client.open_by_key("1B03IEnfOo3pAG7wBIjDW6jIHP0CTzn7jQJuxlNJeb
 stars_on_sheet = client.open_by_key("1B03IEnfOo3pAG7wBIjDW6jIHP0CTzn7jQJuxlNJebgc").worksheet("STARS-ON")
 off_log_sheet = client.open_by_key("1B03IEnfOo3pAG7wBIjDW6jIHP0CTzn7jQJuxlNJebgc").worksheet("Logs-OFF")
 on_log_sheet = client.open_by_key("1B03IEnfOo3pAG7wBIjDW6jIHP0CTzn7jQJuxlNJebgc").worksheet("Logs-ON")
-rating_list = list(map(int, stars_off_sheet.col_values(5)[1:]))
+off_rating_list = list(map(int, stars_off_sheet.col_values(5)[1:]))
+on_rating_list = list(map(int, stars_on_sheet.col_values(5)[1:]))
 
 PERCENTILE_RANGE = 0.10
+MM_CHANNEL_ID = 968987148357349399
 queue = {}
 
 
@@ -37,11 +39,13 @@ async def on_ready():
 
 
 # Command for a player to enter the matchmaking queue
+# If they are in the queue already, it will refresh their presence in the queue
+# You can also move from one queue to another with this
 @bot.command(name="queue", aliases=["q"], help="Enter queue")
 async def enter_queue(ctx, game_type="ranked"):
     await ctx.message.delete()
     if game_type.lower() not in ["ranked", "unranked", "stars"]:
-        await ctx.send("Invalid queue type")
+        await ctx.send("Invalid game type")
         return False
     player_rating = 1400
     player_id = str(ctx.author.id)
@@ -57,7 +61,7 @@ async def enter_queue(ctx, game_type="ranked"):
     queue[player_id] = {"Name": player_name, "Rating": player_rating, "Time": time.time(), "Game Type": game_type}
 
     # calculate search range
-    min_rating, max_rating = calc_search_range(player_rating, PERCENTILE_RANGE)
+    min_rating, max_rating = calc_search_range(player_rating, game_type.lower(), PERCENTILE_RANGE)
 
     # check for match
     await check_for_match(player_id, min_rating, max_rating, 0)
@@ -65,6 +69,8 @@ async def enter_queue(ctx, game_type="ranked"):
     await post_queue_status(ctx.channel.id)
 
 
+# Command for a player to remove themselves from the queue
+# If they aren't in the queue, it will just post a message with the queue status
 @bot.command(name="dequeue", aliases=["dq"], help="Exit queue")
 async def exit_queue(ctx):
     await ctx.message.delete()
@@ -82,21 +88,23 @@ async def refresh_queue():
             new_range = PERCENTILE_RANGE * time_in_queue / 120
             min_rating, max_rating = calc_search_range(queue[player]["Rating"], new_range)
             if await check_for_match(player, min_rating, max_rating, 120):
-                post_queue_status(968987148357349399)
+                post_queue_status(MM_CHANNEL_ID)
                 break
 
 
 # update spreadsheet API data once per minute
 @tasks.loop(minutes=1)
 async def refresh_api_data():
-    global stars_off_sheet, stars_on_sheet, off_log_sheet, on_log_sheet, rating_list
+    global stars_off_sheet, stars_on_sheet, off_log_sheet, on_log_sheet, off_rating_list, on_rating_list
     stars_off_sheet = client.open_by_key("1B03IEnfOo3pAG7wBIjDW6jIHP0CTzn7jQJuxlNJebgc").worksheet("STARS-OFF")
     stars_on_sheet = client.open_by_key("1B03IEnfOo3pAG7wBIjDW6jIHP0CTzn7jQJuxlNJebgc").worksheet("STARS-ON")
     off_log_sheet = client.open_by_key("1B03IEnfOo3pAG7wBIjDW6jIHP0CTzn7jQJuxlNJebgc").worksheet("Logs-OFF")
     on_log_sheet = client.open_by_key("1B03IEnfOo3pAG7wBIjDW6jIHP0CTzn7jQJuxlNJebgc").worksheet("Logs-ON")
-    rating_list = list(map(int, stars_off_sheet.col_values(5)[1:]))
+    off_rating_list = list(map(int, stars_off_sheet.col_values(5)[1:]))
+    on_rating_list = list(map(int, stars_on_sheet.col_values(5)[1:]))
 
 
+# Send a message with the current queue status to the designated channel
 async def post_queue_status(channel_id):
     channel = bot.get_channel(channel_id)
     ranked_q = unranked_q = stars_q = 0
@@ -108,13 +116,16 @@ async def post_queue_status(channel_id):
         if queue[user]["Game Type"] == "stars":
             stars_q += 1
     print(queue)
-    await channel.send("There are " + str(len(queue)) + " users in the matchmaking queue (" + str(ranked_q) + " ranked, " + str(unranked_q) + " unranked, " + str("stars") + " stars-on)")
+    await channel.send("There are " + str(len(queue)) + " users in the matchmaking queue (" + str(ranked_q) + " ranked, " + str(unranked_q) + " unranked, " + str(stars_q) + " stars-on)")
 
 
 # params: player's rating and what percentile you want your search range to cover
 # return: min and max rating the player can match against
-def calc_search_range(rating, percentile):
-    rating_list_copy = rating_list.copy()
+def calc_search_range(rating, game_type, percentile):
+    if game_type == "stars":
+        rating_list_copy = on_rating_list.copy()
+    else:
+        rating_list_copy = off_rating_list.copy()
     rating_list_copy.append(rating)
     pct_list = sorted(rating_list_copy, reverse=True)
     max_index = round(pct_list.index(rating) - (len(pct_list) * percentile))
@@ -134,7 +145,7 @@ def calc_search_range(rating, percentile):
 # Uses their user_id, search range (min-max ratings), and the min time an opponent must be searching to be matched.
 async def check_for_match(user_id, min_rating, max_rating, min_time):
     print("Rating:", queue[user_id]["Rating"], "Time:", round(time.time() - queue[user_id]["Time"]), "Rating Range", min_rating, max_rating)
-    channel = bot.get_channel(968987148357349399)
+    channel = bot.get_channel(MM_CHANNEL_ID)
     if len(queue) >= 2:
         best_match = False
         for player in queue:
